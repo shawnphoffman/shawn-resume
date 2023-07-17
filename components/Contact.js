f9d3de9364c23774c76722b355b94e1fceb00fe4
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-17 15:39:47 -0700

    Fix contact row

--- components/Contact.js
+++ components/Contact.js
@@ -2,6 +2,7 @@
 
 import { memo } from 'react'
 
+import styles from 'app/Global.module.css'
 import useDetectPrint from 'hooks/useDetectPrint'
 
 const Contact = () => {
@@ -10,9 +11,11 @@ const Contact = () => {
 	if (!isPrinting) return null
 	return (
 		<>
-			<a href="https://shawn.party">https://shawn.party</a>
-			<div>{window.atob('c2hhd24ucC5ob2ZmbWFuQGdtYWlsLmNvbQ')}</div>
-			<div>{window.atob('MzE3LjYwNC4wMzI1')}</div>
+			<a className={styles.contact} href="https://shawn.party">
+				https://shawn.party
+			</a>
+			<div className={styles.contact}>{window.atob('c2hhd24ucC5ob2ZmbWFuQGdtYWlsLmNvbQ')}</div>
+			<div className={styles.contact}>{window.atob('MzE3LjYwNC4wMzI1')}</div>
 		</>
 	)
 }
