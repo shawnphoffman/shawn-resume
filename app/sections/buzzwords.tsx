b99d9a9f4c98b4f0dd474510a8a04d393ad406d7
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-10-15 13:05:37 -0700

    Styling updates and revamp buzzwords

--- app/sections/buzzwords.tsx
+++ app/sections/buzzwords.tsx
@@ -6,22 +6,78 @@ export default function Buzzwords() {
 			<h2 className={styles.sectionHeader}>Buzzwords</h2>
 			<div className={styles.columnedLists}>
 				<ul className={styles.bulletedList}>
-					<li>JavaScript / TypeScript / React / NextJS</li>
-					<li>Postgres / SQLite / Redis / KV</li>
-					<li>REST / GraphQL / OAuth / OWASP</li>
-					<li>HTML / CSS / Tailwind / shadcn</li>
-				</ul>
-				<ul className={styles.bulletedList}>
-					<li>NodeJS / Docker / nginx / Caddy</li>
-					<li>React Native / Expo / Electron</li>
-					<li>Cloudflare / S3 / CloudFront / Azure</li>
-					<li>Webpack / Jest / Enzyme / Storybook</li>
-				</ul>
-				<ul className={styles.bulletedList}>
-					<li>Git / VSCode / AI / OpenAI / Cursor</li>
-					<li>Svelte / SvelteKit / Tailwind / Vite </li>
-					<li>C# / .NET Core / .NET Standard / MVC</li>
-					<li>Framer / Figma / Adobe Creative Suite</li>
+					<li>
+						<span>JavaScript</span>
+						<span>TypeScript</span>
+						<span>React</span>
+						<span>NextJS</span>
+					</li>
+					<li>
+						<span>Postgres</span>
+						<span>SQLite</span>
+						<span>Redis</span>
+						<span>KV</span>
+					</li>
+					<li>
+						<span>REST</span>
+						<span>GraphQL</span>
+						<span>OAuth</span>
+						<span>OWASP</span>
+					</li>
+					<li>
+						<span>HTML</span>
+						<span>CSS</span>
+						<span>Tailwind</span>
+						<span>shadcn</span>
+					</li>
+					<li>
+						<span>NodeJS</span>
+						<span>Docker</span>
+						<span>nginx</span>
+						<span>Caddy</span>
+					</li>
+					<li>
+						<span>React Native</span>
+						<span>Expo</span>
+						<span>Electron</span>
+						<span>Cloudflare</span>
+					</li>
+					<li>
+						<span>Cloudflare</span>
+						<span>S3</span>
+						<span>CloudFront</span>
+						<span>Azure</span>
+					</li>
+					<li>
+						<span>Webpack</span>
+						<span>Jest</span>
+						<span>Enzyme</span>
+						<span>Storybook</span>
+					</li>
+					<li>
+						<span>Git</span>
+						<span>VSCode</span>
+						<span>AI</span>
+						<span>OpenAI</span>
+						<span>Cursor</span>
+					</li>
+					<li>
+						<span>Svelte</span>
+						<span>SvelteKit</span>
+						<span>Tailwind</span>
+						<span>Vite</span>
+					</li>
+					<li>
+						<span>C#</span>
+						<span>.NET Core</span>
+						<span>.NET Standard</span>
+						<span>MVC</span>
+					</li>
+					<li>
+						<span>Framer</span>
+						<span>Figma</span>
+						<span>Adobe Creative Suite</span>
+					</li>
 				</ul>
 			</div>
 		</>
